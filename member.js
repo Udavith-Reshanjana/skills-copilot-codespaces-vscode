665f8e0f7d518ef0a5925ee4d35b7a36b9dd3719
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2024-04-20 17:17:52 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,13 +1,3 @@
 function skillMember() {
-    var member = {
-        name: "John",
-        age: 30,
-        skills: ["C++", "Java", "Python"],
-        address: {
-            city: "New York",
-            country: "USA"
-        }
-        
-    };
-    return member;
+  console.log('skillMember');
 }
